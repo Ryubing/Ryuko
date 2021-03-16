f09bdf12793905f6ac73827a4f979d255415cc47
Mark Araujo <maraujo123@hotmail.co.uk>
2021-03-16 17:39:04 +0000

    Refactored embed generation to make more sense - Embed is now based off a generic json assuming Unknown values at first - Embed fields moved closer together - Fields with newlines joined instead of manually separated - Intel iGPU message changed to show preference for discrete GPU's

--- cogs/logfilereader.py
+++ cogs/logfilereader.py
@@ -30,7 +30,7 @@ class LogFileReader(Cog):
         attached_log = message.attachments[0]
         author = f"@{message.author.name}"
         log_file = await self.download_file(attached_log.url)
-        # Large files show a header value when not downlodaed completely
+        # Large files show a header value when not downloaded completely
         # this regex makes sure that the log text to read starts from the first timestamp, ignoring headers
         log_file_header_regex = re.compile(r"\d{2}:\d{2}:\d{2}\.\d{3}.*", re.DOTALL)
         log_file = re.search(log_file_header_regex, log_file).group(0)
@@ -72,25 +72,21 @@ class LogFileReader(Cog):
                 return mods_status
 
         def generate_log_embed(author_id, log_info):
-            cleaned_game_name = re.sub(r"\s\[(64|32)-bit\]$", "", log_info["Game_Name"])
-            log_embed = discord.Embed(
-                title=f"{cleaned_game_name}", colour=discord.Colour(0x4A90E2)
-            )
-            log_embed.set_footer(text=f"Log uploaded by {author_id}")
-            log_embed.add_field(
-                name="Hardware Info",
-                value=f"**CPU:** {log_info['CPU']}\n**GPU:** {log_info['GPU']}\n**RAM:** {log_info['RAM']}\n**OS:** {log_info['OS']}",
-            )
-            log_embed.add_field(
-                name="Ryujinx Info",
-                value=f"**Version:** {log_info['Ryu_Version']}\n**Firmware:** {log_info['Firmware']}",
-            )
-            if log_info["Game_Name"] == "Unknown":
-                log_embed.add_field(
-                    name="Empty Log",
-                    value=f"This log file appears to be empty. To get a proper log, follow these steps:\n 1) Start a game up.\n 2) Play until your issue occurs.\n 3) Upload your log file.",
-                    inline=False,
-                )
+            embed = {
+                "hardware_info": {
+                    "cpu": "Unknown",
+                    "gpu": "Unknown",
+                    "ram": "Unknown",
+                    "os": "Unknown",
+                },
+                "emu_info": {"ryu_version": "Unknown", "ryu_firmware": "Unknown"},
+                "game_info": {
+                    "game_name": "Unknown",
+                    "errors": "No errors found in log",
+                    "mods": "No mods found",
+                    "notes": [],
+                },
+            }
 
             def find_error_message(log_file):
                 try:
@@ -114,25 +110,16 @@ class LogFileReader(Cog):
             # Finds the lastest error denoted by |E| in the log and its first line
             error_message = find_error_message(log_file)
             if error_message:
-                error_info = f"```{error_message}```"
+                embed["game_info"]["errors"] = f"```{error_message}```"
             else:
-                error_info = "No errors found in log"
-            log_embed.add_field(
-                name="Latest Error Snippet",
-                value=error_info,
-                inline=False,
-            )
+                pass
 
             # Find information on installed mods
             game_mods = mods_information(log_file)
             if game_mods:
-                mods_info = "\n".join(game_mods)
+                embed["game_info"]["mods"] = "\n".join(game_mods)
             else:
-                mods_info = "No mods found"
-            log_embed.add_field(name="Mods", value=mods_info, inline=False)
-
-            # Generic checks to notify user about
-            notes = []
+                pass
 
             controllers_regex = re.compile(r"Hid Configure: ([^\r\n]+)")
             controllers = re.findall(controllers_regex, log_file)
@@ -141,25 +128,25 @@ class LogFileReader(Cog):
                 input_string = "\n".join(input_status)
             else:
                 input_string = "⚠️ No controller information found"
-            notes.append(input_string)
+            embed["game_info"]["notes"].append(input_string)
 
             try:
                 ram_avaliable_regex = re.compile(r"Available\s(\d+)(?=\sMB)")
                 ram_avaliable = re.search(ram_avaliable_regex, log_file)[1]
                 if int(ram_avaliable) < 8000:
-                    ram_warning = f"⚠️ Less than 8GB RAM avaliable"
-                    notes.append(ram_warning)
+                    ram_warning = "⚠️ Less than 8GB RAM avaliable"
+                    embed["game_info"]["notes"].append(ram_warning)
             except TypeError:
                 pass
 
             if "Darwin" in log_info["OS"]:
-                mac_os_warning = f"**❌ macOS is currently unsupported**"
-                notes.append(mac_os_warning)
+                mac_os_warning = "**❌ macOS is currently unsupported**"
+                embed["game_info"]["notes"].append(mac_os_warning)
 
             if "Intel" in log_info["GPU"]:
                 if "Darwin" in log_info["OS"] or "Windows" in log_info["OS"]:
-                    intel_gpu_warning = f"**❌ Intel iGPUs are not supported**"
-                    notes.append(intel_gpu_warning)
+                    intel_gpu_warning = "**⚠️ Intel iGPUs are known to have driver issues, consider using a discrete GPU**"
+                    embed["game_info"]["notes"].append(intel_gpu_warning)
 
             # Find information on logs, whether defaults are enabled or not
             default_logs = ["Info", "Warning", "Error", "Guest", "Stub"]
@@ -170,12 +157,63 @@ class LogFileReader(Cog):
                 log_string = "\n".join(logs_status)
             else:
                 log_string = "✅ Default logs enabled"
-            notes.append(log_string)
+            embed["game_info"]["notes"].append(log_string)
+
+            embed["hardware_info"]["cpu"] = log_info["CPU"]
+            embed["hardware_info"]["gpu"] = log_info["GPU"]
+            embed["hardware_info"]["ram"] = log_info["RAM"]
+            embed["hardware_info"]["os"] = log_info["OS"]
+            embed["emu_info"]["ryu_version"] = log_info["Ryu_Version"]
+            embed["emu_info"]["ryu_firmware"] = log_info["Firmware"]
+
+            cleaned_game_name = re.sub(r"\s\[(64|32)-bit\]$", "", log_info["Game_Name"])
+            embed["game_info"]["game_name"] = cleaned_game_name
+
+            hardware_info = "\n".join(
+                (
+                    f"**CPU:** {embed['hardware_info']['cpu']}",
+                    f"**GPU:** {embed['hardware_info']['gpu']}",
+                    f"**RAM:** {embed['hardware_info']['ram']}",
+                    f"**OS:** {embed['hardware_info']['os']}",
+                )
+            )
+
+            ryujinx_info = "\n".join(
+                (
+                    f"**Version:** {embed['emu_info']['ryu_version']}",
+                    f"**Firmware:** {embed['emu_info']['ryu_firmware']}",
+                )
+            )
+
+            log_embed = discord.Embed(
+                title=f"{cleaned_game_name}", colour=discord.Colour(0x4A90E2)
+            )
+            log_embed.set_footer(text=f"Log uploaded by {author_id}")
+            log_embed.add_field(name="Hardware Info", value=hardware_info)
+            log_embed.add_field(name="Ryujinx Info", value=ryujinx_info)
+            if log_info["Game_Name"] == "Unknown":
+                log_embed.add_field(
+                    name="Empty Log",
+                    value="""This log file appears to be empty. To get a proper log, follow these steps:
+                    \n 1) Start a game up.
+                    \n 2) Play until your issue occurs.
+                    \n 3) Upload your log file.""",
+                    inline=False,
+                )
             log_embed.add_field(
-                name="Notes",
-                value="\n".join([note for note in notes]),
+                name="Latest Error Snippet",
+                value=embed["game_info"]["errors"],
                 inline=False,
             )
+            log_embed.add_field(
+                name="Mods", value=embed["game_info"]["mods"], inline=False
+            )
+            log_embed.add_field(
+                name="Notes",
+                value="\n".join([note for note in embed["game_info"]["notes"]]),
+                inline=False,
+            )
+
             return log_embed
 
         system_info = specs_search(log_file)
@@ -191,6 +229,7 @@ class LogFileReader(Cog):
         try:
             author = message.author.mention
             filename = message.attachments[0].filename
+            # Any message over 2000 chars is uploaded as message.txt, so this is accounted for
             log_file_regex = re.compile(r"^Ryujinx_.*\.log|message\.txt$")
             is_log_file = re.match(log_file_regex, filename)
             if message.channel.id in self.bot_log_allowed_channels and is_log_file:
