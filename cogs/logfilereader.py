125b41b0cc4bf40749bfbc0e9961ab9c35cf6dd3
Mark Araujo <maraujo123@hotmail.co.uk>
2021-03-18 13:27:27 +0000

    Refactor to be simpler and easier to read. - Hardware, ryujinx and log analysis split into separate functions - Regex explicitly defined for each property instead of confusing map - Added user settings reported, shows PPTC enabled or disabled

--- cogs/logfilereader.py
+++ cogs/logfilereader.py
@@ -1,5 +1,5 @@
-from inspect import Traceback
 import logging
+from typing import Type
 import discord
 from discord.ext.commands import Cog
 import re
@@ -27,66 +27,160 @@ class LogFileReader(Cog):
                 return await response.text("UTF-8")
 
     async def log_file_read(self, message):
+        self.embed = {
+            "hardware_info": {
+                "cpu": "Unknown",
+                "gpu": "Unknown",
+                "ram": "Unknown",
+                "os": "Unknown",
+            },
+            "emu_info": {
+                "ryu_version": "Unknown",
+                "ryu_firmware": "Unknown",
+                "logs_enabled": None,
+            },
+            "game_info": {
+                "game_name": "Unknown",
+                "errors": "No errors found in log",
+                "mods": "No mods found",
+                "notes": [],
+            },
+            "user_settings": {
+                "pptc": "Unknown",
+            },
+        }
         attached_log = message.attachments[0]
-        author = f"@{message.author.name}"
+        author_name = f"@{message.author.name}"
         log_file = await self.download_file(attached_log.url)
         # Large files show a header value when not downloaded completely
         # this regex makes sure that the log text to read starts from the first timestamp, ignoring headers
         log_file_header_regex = re.compile(r"\d{2}:\d{2}:\d{2}\.\d{3}.*", re.DOTALL)
         log_file = re.search(log_file_header_regex, log_file).group(0)
 
-        regex_map = {
-            "Ryu_Version": "Ryujinx Version",
-            "Firmware": "Firmware Version",
-            "OS": "Operating System",
-            "CPU": "CPU",
-            "GPU": "PrintGpuInformation",
-            "RAM": "RAM",
-            "Game_Name": "Loader LoadNca: Application Loaded",
-            "Logs_Enabled": "Logs Enabled",
-        }
+        def get_hardware_info(log_file):
+            try:
+                self.embed["hardware_info"]["cpu"] = (
+                    re.search(r"CPU:\s([^;\r]*)", log_file, re.MULTILINE)
+                    .group(1)
+                    .rstrip()
+                )
+                self.embed["hardware_info"]["ram"] = (
+                    re.search(r"RAM:(\sTotal)?\s([^;\r]*)", log_file, re.MULTILINE)
+                    .group(2)
+                    .rstrip()
+                )
+                self.embed["hardware_info"]["os"] = (
+                    re.search(r"Operating System:\s([^;\r]*)", log_file, re.MULTILINE)
+                    .group(1)
+                    .rstrip()
+                )
+                self.embed["hardware_info"]["gpu"] = (
+                    re.search(
+                        r"PrintGpuInformation:\s([^;\r]*)", log_file, re.MULTILINE
+                    )
+                    .group(1)
+                    .rstrip()
+                )
+            except AttributeError:
+                pass
 
-        def make_regex_obj(key, value):
-            # Since RAM values in log are structured as "RAM: Total 16297 MB ;", regex tries to account for the unneeded 'Total' string
-            return re.compile(fr"{value}:(\sTotal)?\s(?P<{key}>[^;\r]*)", re.MULTILINE)
+        def get_ryujinx_info(log_file):
+            self.embed["emu_info"]["ryu_version"] = (
+                re.search(r"Ryujinx Version:\s([^;\r]*)", log_file, re.MULTILINE)
+                .group(1)
+                .rstrip()
+            )
+            self.embed["emu_info"]["ryu_firmware"] = (
+                re.search(r"Firmware Version:\s([^;\r]*)", log_file, re.MULTILINE)
+                .group(1)
+                .rstrip()
+            )
+            self.embed["emu_info"]["logs_enabled"] = (
+                re.search(r"Logs Enabled:\s([^;\r]*)", log_file, re.MULTILINE)
+                .group(1)
+                .rstrip()
+            )
 
-        def find_specs(body, key, regexobj):
-            match = regexobj.search(body)
-            return match.group(key).rstrip() if match is not None else "Unknown"
+        def format_log_embed():
+            cleaned_game_name = re.sub(
+                r"\s\[(64|32)-bit\]$", "", self.embed["game_info"]["game_name"]
+            )
+            self.embed["game_info"]["game_name"] = cleaned_game_name
 
-        def specs_search(body):
-            return {
-                key: find_specs(body, key, make_regex_obj(key, value))
-                for key, value in regex_map.items()
-            }
+            hardware_info = "\n".join(
+                (
+                    f"**CPU:** {self.embed['hardware_info']['cpu']}",
+                    f"**GPU:** {self.embed['hardware_info']['gpu']}",
+                    f"**RAM:** {self.embed['hardware_info']['ram']}",
+                    f"**OS:** {self.embed['hardware_info']['os']}",
+                )
+            )
 
-        def mods_information(log_file):
-            mods_regex = re.compile(r"Found mod\s\'(.+?)\'\s(\[.+?\])")
-            matches = re.findall(mods_regex, log_file)
-            if matches:
-                mods = [{"mod": match[0], "status": match[1]} for match in matches]
-                mods_status = [
-                    f"ℹ️ {i['mod']} ({'ExeFS' if i['status'] == '[E]' else 'RomFS'})"
-                    for i in mods
-                ]
-                return mods_status
+            user_settings_info = "\n".join(
+                (f"**PPTC:** {self.embed['user_settings']['pptc']}",)
+            )
 
-        def generate_log_embed(author_id, log_info):
-            embed = {
-                "hardware_info": {
-                    "cpu": "Unknown",
-                    "gpu": "Unknown",
-                    "ram": "Unknown",
-                    "os": "Unknown",
-                },
-                "emu_info": {"ryu_version": "Unknown", "ryu_firmware": "Unknown"},
-                "game_info": {
-                    "game_name": "Unknown",
-                    "errors": "No errors found in log",
-                    "mods": "No mods found",
-                    "notes": [],
-                },
-            }
+            ryujinx_info = "\n".join(
+                (
+                    f"**Version:** {self.embed['emu_info']['ryu_version']}",
+                    f"**Firmware:** {self.embed['emu_info']['ryu_firmware']}",
+                )
+            )
+
+            log_embed = discord.Embed(
+                title=f"{cleaned_game_name}", colour=discord.Colour(0x4A90E2)
+            )
+            log_embed.set_footer(text=f"Log uploaded by {author_name}")
+            log_embed.add_field(name="Hardware Info", value=hardware_info)
+            log_embed.add_field(name="Ryujinx Info", value=ryujinx_info)
+            if cleaned_game_name == "Unknown":
+                log_embed.add_field(
+                    name="Empty Log",
+                    value="""This log file appears to be empty. To get a proper log, follow these steps:
+                        \n 1) Start a game up.
+                        \n 2) Play until your issue occurs.
+                        \n 3) Upload your log file.""",
+                    inline=False,
+                )
+            log_embed.add_field(
+                name="Settings",
+                value=user_settings_info,
+                inline=False,
+            )
+            log_embed.add_field(
+                name="Latest Error Snippet",
+                value=self.embed["game_info"]["errors"],
+                inline=False,
+            )
+            log_embed.add_field(
+                name="Mods", value=self.embed["game_info"]["mods"], inline=False
+            )
+            log_embed.add_field(
+                name="Notes",
+                value="\n".join([note for note in self.embed["game_info"]["notes"]]),
+                inline=False,
+            )
+
+            return log_embed
+
+        def analyse_log(log_file):
+            self.embed["game_info"]["game_name"] = (
+                re.search(
+                    r"Loader LoadNca: Application Loaded:\s([^;\r]*)",
+                    log_file,
+                    re.MULTILINE,
+                )
+                .group(1)
+                .rstrip()
+            )
+
+            pptc_setting = re.search(
+                r"Ptc Initialize:.+\(enabled:\s(.+?)\)[^;\r]", log_file, re.MULTILINE
+            ).group(1)
+            if pptc_setting == "True":
+                self.embed["user_settings"]["pptc"] = "Enabled"
+            else:
+                self.embed["user_settings"]["pptc"] = "Disabled"
 
             def find_error_message(log_file):
                 try:
@@ -110,14 +204,25 @@ class LogFileReader(Cog):
             # Finds the lastest error denoted by |E| in the log and its first line
             error_message = find_error_message(log_file)
             if error_message:
-                embed["game_info"]["errors"] = f"```{error_message}```"
+                self.embed["game_info"]["errors"] = f"```{error_message}```"
             else:
                 pass
+
+            def mods_information(log_file):
+                mods_regex = re.compile(r"Found mod\s\'(.+?)\'\s(\[.+?\])")
+                matches = re.findall(mods_regex, log_file)
+                if matches:
+                    mods = [{"mod": match[0], "status": match[1]} for match in matches]
+                    mods_status = [
+                        f"ℹ️ {i['mod']} ({'ExeFS' if i['status'] == '[E]' else 'RomFS'})"
+                        for i in mods
+                    ]
+                    return mods_status
 
             # Find information on installed mods
             game_mods = mods_information(log_file)
             if game_mods:
-                embed["game_info"]["mods"] = "\n".join(game_mods)
+                self.embed["game_info"]["mods"] = "\n".join(game_mods)
             else:
                 pass
 
@@ -128,96 +233,50 @@ class LogFileReader(Cog):
                 input_string = "\n".join(input_status)
             else:
                 input_string = "⚠️ No controller information found"
-            embed["game_info"]["notes"].append(input_string)
+            self.embed["game_info"]["notes"].append(input_string)
 
             try:
                 ram_avaliable_regex = re.compile(r"Available\s(\d+)(?=\sMB)")
                 ram_avaliable = re.search(ram_avaliable_regex, log_file)[1]
                 if int(ram_avaliable) < 8000:
                     ram_warning = "⚠️ Less than 8GB RAM avaliable"
-                    embed["game_info"]["notes"].append(ram_warning)
+                    self.embed["game_info"]["notes"].append(ram_warning)
             except TypeError:
                 pass
 
-            if "Darwin" in log_info["OS"]:
+            if "Darwin" in self.embed["hardware_info"]["os"]:
                 mac_os_warning = "**❌ macOS is currently unsupported**"
-                embed["game_info"]["notes"].append(mac_os_warning)
+                self.embed["game_info"]["notes"].append(mac_os_warning)
 
-            if "Intel" in log_info["GPU"]:
-                if "Darwin" in log_info["OS"] or "Windows" in log_info["OS"]:
+            if "Intel" in self.embed["hardware_info"]["gpu"]:
+                if (
+                    "Darwin" in self.embed["hardware_info"]["os"]
+                    or "Windows" in self.embed["hardware_info"]["os"]
+                ):
                     intel_gpu_warning = "**⚠️ Intel iGPUs are known to have driver issues, consider using a discrete GPU**"
-                    embed["game_info"]["notes"].append(intel_gpu_warning)
+                    self.embed["game_info"]["notes"].append(intel_gpu_warning)
 
             # Find information on logs, whether defaults are enabled or not
             default_logs = ["Info", "Warning", "Error", "Guest", "Stub"]
-            user_logs = log_info["Logs_Enabled"].rstrip().replace(" ", "").split(",")
+            user_logs = (
+                self.embed["emu_info"]["logs_enabled"]
+                .rstrip()
+                .replace(" ", "")
+                .split(",")
+            )
             disabled_logs = set(default_logs).difference(set(user_logs))
             if disabled_logs:
                 logs_status = [f"⚠️ {log} log is not enabled" for log in disabled_logs]
                 log_string = "\n".join(logs_status)
             else:
                 log_string = "✅ Default logs enabled"
-            embed["game_info"]["notes"].append(log_string)
+            self.embed["game_info"]["notes"].append(log_string)
 
-            embed["hardware_info"]["cpu"] = log_info["CPU"]
-            embed["hardware_info"]["gpu"] = log_info["GPU"]
-            embed["hardware_info"]["ram"] = log_info["RAM"]
-            embed["hardware_info"]["os"] = log_info["OS"]
-            embed["emu_info"]["ryu_version"] = log_info["Ryu_Version"]
-            embed["emu_info"]["ryu_firmware"] = log_info["Firmware"]
+        get_hardware_info(log_file)
+        get_ryujinx_info(log_file)
+        analyse_log(log_file)
 
-            cleaned_game_name = re.sub(r"\s\[(64|32)-bit\]$", "", log_info["Game_Name"])
-            embed["game_info"]["game_name"] = cleaned_game_name
-
-            hardware_info = "\n".join(
-                (
-                    f"**CPU:** {embed['hardware_info']['cpu']}",
-                    f"**GPU:** {embed['hardware_info']['gpu']}",
-                    f"**RAM:** {embed['hardware_info']['ram']}",
-                    f"**OS:** {embed['hardware_info']['os']}",
-                )
-            )
-
-            ryujinx_info = "\n".join(
-                (
-                    f"**Version:** {embed['emu_info']['ryu_version']}",
-                    f"**Firmware:** {embed['emu_info']['ryu_firmware']}",
-                )
-            )
-
-            log_embed = discord.Embed(
-                title=f"{cleaned_game_name}", colour=discord.Colour(0x4A90E2)
-            )
-            log_embed.set_footer(text=f"Log uploaded by {author_id}")
-            log_embed.add_field(name="Hardware Info", value=hardware_info)
-            log_embed.add_field(name="Ryujinx Info", value=ryujinx_info)
-            if log_info["Game_Name"] == "Unknown":
-                log_embed.add_field(
-                    name="Empty Log",
-                    value="""This log file appears to be empty. To get a proper log, follow these steps:
-                    \n 1) Start a game up.
-                    \n 2) Play until your issue occurs.
-                    \n 3) Upload your log file.""",
-                    inline=False,
-                )
-            log_embed.add_field(
-                name="Latest Error Snippet",
-                value=embed["game_info"]["errors"],
-                inline=False,
-            )
-            log_embed.add_field(
-                name="Mods", value=embed["game_info"]["mods"], inline=False
-            )
-            log_embed.add_field(
-                name="Notes",
-                value="\n".join([note for note in embed["game_info"]["notes"]]),
-                inline=False,
-            )
-
-            return log_embed
-
-        system_info = specs_search(log_file)
-        return generate_log_embed(author, system_info)
+        return format_log_embed()
 
     @Cog.listener()
     async def on_message(self, message):
@@ -227,7 +286,7 @@ class LogFileReader(Cog):
 
         # If message has an attachment
         try:
-            author = message.author.mention
+            author_mention = message.author.mention
             filename = message.attachments[0].filename
             # Any message over 2000 chars is uploaded as message.txt, so this is accounted for
             log_file_regex = re.compile(r"^Ryujinx_.*\.log|message\.txt$")
@@ -249,7 +308,7 @@ class LogFileReader(Cog):
                         return await reply_message.edit(content=None, embed=embed)
                     except UnicodeDecodeError:
                         return await message.channel.send(
-                            f"This log file appears to be invalid {author}. Please re-check and re-upload your log file."
+                            f"This log file appears to be invalid {author_mention}. Please re-check and re-upload your log file."
                         )
                     except Exception as error:
                         await reply_message.edit(
@@ -258,15 +317,15 @@ class LogFileReader(Cog):
                         print(logging.warn(error))
                 else:
                     await message.channel.send(
-                        f"The log file `{filename}` appears to be a duplicate {author}. Please upload a more recent file."
+                        f"The log file `{filename}` appears to be a duplicate {author_mention}. Please upload a more recent file."
                     )
             elif not is_log_file:
                 return await message.channel.send(
-                    f"{author} Your file does not match the Ryujinx log format. Please check your file."
+                    f"{author_mention} Your file does not match the Ryujinx log format. Please check your file."
                 )
             else:
                 return await message.channel.send(
-                    f"{author} Please upload log files to {' or '.join([f'<#{id}>' for id in self.bot_log_allowed_channels])}"
+                    f"{author_mention} Please upload log files to {' or '.join([f'<#{id}>' for id in self.bot_log_allowed_channels])}"
                 )
         except IndexError:
             pass
