c082b3a964cd50cf7c7e9c6f2581ccc28c7481e2
Mark Araujo <maraujo123@hotmail.co.uk>
2022-01-21 17:06:35 +0000

    Explains server rules

--- robocop_ng/cogs/explain.py
+++ robocop_ng/cogs/explain.py
@@ -11,8 +11,8 @@ class Explainer(Cog):
         self.explanations = {
             "shaders": {
                 "body_text": """Shaders are small programs running on a graphic card that are responsible for rendering graphics like terrain, characters, explosions, grass etc. Since a PC cannot directly execute Switch shaders these have to be translated into a format a PC can understand. This translation process is time consuming and you'll notice it in two ways:
-                                \n1). Any new shaders that are encountered will cause the emulation to pause until the translation is done, which causes stuttering and FPS drops. When you load a game for the first time a lot of translation will happen, but by playing more this stuttering decreases rapidly.
-                                \n2). Ryujinx will save any shaders a game uses (aka it caches them). When you launch a game Ryujinx will take some time loading those saved shaders to prevent the stuttering from happening again.
+                                \n1) Any new shaders that are encountered will cause the emulation to pause until the translation is done, which causes stuttering and FPS drops. When you load a game for the first time a lot of translation will happen, but by playing more this stuttering decreases rapidly.
+                                \n2) Ryujinx will save any shaders a game uses (aka it caches them). When you launch a game Ryujinx will take some time loading those saved shaders to prevent the stuttering from happening again.
                                 \nFor a more technical explanation see here: https://blog.ryujinx.org/shader-cache-is-finally-here/
                                 \n**NOTE: Shaders contain copyrighted game code so you must _generate your own_. Using third-party shaders is considered piracy.**""",
                 "title": "Shaders & Shader Caches explained",
@@ -70,14 +70,14 @@ class Explainer(Cog):
                             \nIt also often requires emulation of the hardware, so software is used to make a virtual version of the console. This is also very resource intensive (again, mostly on the CPU side).
                             \nNative PC games are optimized to run on modern processors with modern languages and architectures, so there are no performance overheads due to translation.
                             """,
-                "title": "Emulation vs native games",
+                "title": "Emulation vs native PC games",
             },
             "amiibo": {
                 "body_text": """Amiibo data is pulled from AmiiboAPI, so no dumping is needed. To use them in-game:
                 \n1) Go to a section in the game where an Amiibo is requested, usually shown by an icon and a message screen asking to scan an Amiibo.
-                \n2) In Ryujinx, select Tools > Scan an Amiibo.
-                \n3) Choose the Amiibo you want to scan in.
-                \n4) The game should recognize the Amiibo and you should see the effects as though you just scanned a real Amiibo figure.""",
+                2) In Ryujinx, select Tools > Scan an Amiibo.
+                3) Choose the Amiibo you want to scan in.
+                4) The game should recognize the Amiibo and you should see the effects as though you just scanned a real Amiibo figure.""",
                 "image": "https://cdn.gamer-network.net/2020/usgamer/Animal-Crossing-New-Horizons-Photopia-Amiibo.jpg",
                 "title": "Amiibos explained",
             },
@@ -85,8 +85,39 @@ class Explainer(Cog):
                 "body_text": """The Switch has two modes of operation: docked and handheld. Ryujinx can switch between these two.
                                 \nDocked mode: The Switch GPU operates at a higher capacity since it does not have to worry about running off the battery.
                                 This means games run at a higher resolution (1080p native) with some games having extra effects like higher draw distances or more anti-aliasing. This mode is slightly harder to emulate and requires more CPU power.
-                                \n\nHandheld mode: The Switch console downclocks its GPU to save on battery. In this mode games run at 720p native resolution. In terms of emulation, those with underpowered hardware may benefit from this mode as it tends to be comparatively less intensive.""",
+                                \nHandheld mode: The Switch console downclocks its GPU to save on battery. In this mode games run at 720p native resolution. In terms of emulation, those with underpowered hardware may benefit from this mode as it tends to be comparatively less intensive.""",
                 "title": "Docked vs. Handheld mode",
+            },
+            "rule2": {
+                "body_text": "Civil behavior is expected at all times. Disagreements with others are to be occasionally expected; however personal attacks of any kind (including derogatory slurs) are not tolerated and may result in an immediate ban.",
+                "title": "Rule #2",
+            },
+            "rule3": {
+                "body_text": """Do not spam, troll, or post content that is unrelated to the channel you are posting it in. Use common sense.\n
+                                - No NSFW or pirated content.
+                                - No self-advertising.""",
+                "title": "Rule #3",
+            },
+            "rule4": {
+                "body_text": """**This server does not support piracy.**
+                                To use Ryujinx legally you must use your own Switch console and your own games.\n
+                                - No help or support will be given to anyone who uses pirated games, keys, shaders or other files.
+                                - No discussion on leaked games.
+                                - No discussion on how to pirate games, keys, shaders or other files.
+                                - No discussion on why you think piracy should be allowed.
+                                \nWe are not here to argue. Please forward all complaints about copyright law to your local government's copyright enforcement agency.
+                                """,
+                "title": "Rule #4",
+            },
+            "rule5": {
+                "body_text": "No custom builds are to be posted or discussed in the server. Requests for support of a custom build will be denied.",
+                "title": "Rule #5",
+            },
+            "rule6": {
+                "body_text": """Warnings will be issued for violations of the rules, for consistently skirting the rules/attempting to find loopholes for rule breaking, or for disregarding or protesting direction from Staff/Admins.\n
+                                - Three warnings will result in a kick.
+                                - The 4th warning will result in a ban.
+                                - Attempting to evade or mitigate disciplinary action by leaving the server, using alts, etc. will result in an immediate ban."""
             },
         }
 
@@ -148,13 +179,13 @@ class Explainer(Cog):
         name="emulation_explain",
         description="Explains why emulation is different from native gaming.",
     )
-    async def send_embed_emulation(self, ctx: SlashContext):
+    async def send_embed_emulation_explain(self, ctx: SlashContext):
         embed = self.generate_embed("emulation_explain")
         await ctx.send(embed=embed)
 
     @cog_ext.cog_slash(name="amiibo", description="How to use Amiibos in Ryujinx.")
     async def send_embed_amiibo(self, ctx: SlashContext):
-        embed = self.generate_embed("amiibos")
+        embed = self.generate_embed("amiibo")
         embed.set_image(url=self.explanations["amiibo"]["image"])
         await ctx.send(embed=embed)
 
@@ -162,8 +193,33 @@ class Explainer(Cog):
         name="docked_vs_handheld",
         description="Explains difference between docked and handheld modes.",
     )
-    async def send_embed_emulation(self, ctx: SlashContext):
+    async def send_embed_docked_handheld(self, ctx: SlashContext):
         embed = self.generate_embed("docked_handheld_mode")
+        await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="r2", description="Explains civil behavior rule.")
+    async def send_embed_civil(self, ctx: SlashContext):
+        embed = self.generate_embed("rule2")
+        await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="r3", description="Explains content rule.")
+    async def send_embed_troll(self, ctx: SlashContext):
+        embed = self.generate_embed("rule3")
+        await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="r4", description="Explains anti-piracy rule.")
+    async def send_embed_piracy(self, ctx: SlashContext):
+        embed = self.generate_embed("rule4")
+        await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="r5", description="Explains custom builds rule.")
+    async def send_embed_custom_build(self, ctx: SlashContext):
+        embed = self.generate_embed("rule5")
+        await ctx.send(embed=embed)
+
+    @cog_ext.cog_slash(name="r6", description="Explains how warnings work.")
+    async def send_embed_warnings(self, ctx: SlashContext):
+        embed = self.generate_embed("rule6")
         await ctx.send(embed=embed)
 
 
