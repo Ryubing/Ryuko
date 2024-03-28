ce41c97920312d995912bfdbdb1d7b3abda4884b
WilliamWsyHK <WilliamWsyHK@users.noreply.github.com>
2024-03-29 00:15:57 +0800

    Add log parsing capability to capture only enabled mods

--- robocop_ng/helpers/ryujinx_log_analyser.py
+++ robocop_ng/helpers/ryujinx_log_analyser.py
@@ -370,13 +370,14 @@ class LogAnalyser:
                 raise NotImplementedError(key)
 
     def __get_mods(self):
-        mods_regex = re.compile(r"Found mod\s\'(.+?)\'\s(\[.+?\])")
+        mods_regex = re.compile(r"Found\s(enabled|disabled)\smod\s\'(.+?)\'\s(\[.+?\])")
         matches = re.findall(mods_regex, self._log_text)
         if matches:
-            mods = [{"mod": match[0], "status": match[1]} for match in matches]
+            mods = [{"mod": match[1], "status": match[0], "type": match[2]} for match in matches]
             mods_status = [
-                f"ℹ️ {i['mod']} ({'ExeFS' if i['status'] == '[E]' else 'RomFS'})"
+                f"ℹ️ {i['mod']} ({'ExeFS' if i['type'] == '[E]' else 'RomFS'})"
                 for i in mods
+                if i['status'] == 'enabled'
             ]
             # Remove duplicated mods from output
             mods_status = list(dict.fromkeys(mods_status))
