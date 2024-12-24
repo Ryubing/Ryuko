d8cb7987013ef0cc740855297af254d70fc279a6
Evan Husted <greem@greemdev.net>
2024-12-24 13:46:12 -0600

    Revert relying on context config in checks

--- robocop_ng/helpers/checks.py
+++ robocop_ng/helpers/checks.py
@@ -1,13 +1,15 @@
+from robocop_ng import config
+
 def check_if_staff(ctx):
     if not ctx.guild:
         return False
-    return any(r.id in ctx.bot.config.staff_role_ids for r in ctx.author.roles)
+    return any(r.id in config.staff_role_ids for r in ctx.author.roles)
 
 
 def check_if_bot_manager(ctx):
     if not ctx.guild:
         return False
-    return any(r.id == ctx.bot.config.bot_manager_role_id for r in ctx.author.roles)
+    return any(r.id == config.bot_manager_role_id for r in ctx.author.roles)
 
 
 def check_if_staff_or_ot(ctx):
@@ -15,26 +17,25 @@ def check_if_staff_or_ot(ctx):
         return True
     is_ot = ctx.channel.name == "general"
     is_bot_cmds = ctx.channel.name == "bot-spam"
-    is_staff = any(r.id in ctx.bot.config.staff_role_ids for r in ctx.author.roles)
+    is_staff = any(r.id in config.staff_role_ids for r in ctx.author.roles)
     return is_ot or is_staff or is_bot_cmds
 
 
 def check_if_staff_or_dm(ctx):
     if not ctx.guild:
         return True
-    return any(r.id in ctx.bot.config.staff_role_ids for r in ctx.author.roles)
+    return any(r.id in config.staff_role_ids for r in ctx.author.roles)
 
 
 def check_if_collaborator(ctx):
     if not ctx.guild:
         return False
     return any(
-        r.id in ctx.bot.config.staff_role_ids + ctx.bot.config.allowed_pin_roles
+        r.id in config.staff_role_ids + config.allowed_pin_roles
         for r in ctx.author.roles
     )
-
 
 def check_if_pin_channel(ctx):
     if not ctx.guild:
         return False
-    return ctx.message.channel.id in ctx.bot.config.allowed_pin_channels
+    return ctx.message.channel.id in config.allowed_pin_channels
